5c3ad2df51f39749dfb607c8c715edecc8ca0e32
charlottesummers16 <48827234+charlottesummers16@users.noreply.github.com>
2022-02-07 23:27:53 +0000

    Canvas prints on only part of the screen.

--- app/src/main/java/com/summers/artworkeditor/MainActivity.kt
+++ app/src/main/java/com/summers/artworkeditor/MainActivity.kt
@@ -8,55 +8,69 @@ import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
+import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val bitmap: Bitmap = Bitmap.createBitmap(700, 600, Bitmap.Config.ARGB_8888)
+    private val canvas: Canvas = Canvas(bitmap)
+    private var colourTracker = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val bitmap: Bitmap = Bitmap.createBitmap(700, 700, Bitmap.Config.ARGB_8888)
-//        val canvas: Canvas = Canvas(bitmap)
-//
-//        // rectangle positions
-//        var left = 100
-//        var top = 100
-//        var right = 600
-//        var bottom = 400
-//
-//        // draw rectangle shape to canvas
-//        var shapeDrawable: ShapeDrawable = ShapeDrawable(RectShape())
-//        shapeDrawable.setBounds( left, top, right, bottom)
-//        shapeDrawable.paint.color = Color.parseColor("#009944")
-//        shapeDrawable.draw(canvas)
-//
-//
-//        // oval positions
-//        left = 100
-//        top = 500
-//        right = 600
-//        bottom = 800
-//
-//        // draw oval shape to canvas
-//        shapeDrawable = ShapeDrawable(OvalShape())
-//        shapeDrawable.setBounds( left, top, right, bottom)
-//        shapeDrawable.paint.color = Color.parseColor("#009191")
-//        shapeDrawable.draw(canvas)
-//
-//        // set bitmap as background to ImageView
-//        val imageV = findViewById<ImageView>(R.id.imageV)
-//        imageV.background = BitmapDrawable(resources, bitmap)
+        drawShapes("#E60000", "#03A9F4")
 
-        val myCanvasView = MyCanvasView(this)
-        // No XML file; just one custom view created programmatically.
-        // Request the full available screen for layout.
-        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
-        setContentView(myCanvasView)
+        findViewById<Button>(R.id.btnChangeColour).setOnClickListener { changeRectangleColour() }
+//        val view = findViewById<View>(R.id.view)
+//        view.setOnClickListener { changeRectangleColour() }
 
     }
+
+    private fun changeRectangleColour() {
+        colourTracker++
+        if (colourTracker % 2 == 0) {
+            drawShapes("#03A9F4", "#E60000")
+        } else {
+            drawShapes("#E60000", "#03A9F4")
+        }
+    }
+
+    private fun drawShapes(rectangleColor: String, ovalColor: String) {
+        // rectangle positions
+        var left = 200
+        var top = 100
+        var right = 500
+        var bottom = 300
+
+        // draw rectangle shape to canvas
+        var shapeDrawable: ShapeDrawable = ShapeDrawable(RectShape())
+        shapeDrawable.setBounds( left, top, right, bottom)
+        shapeDrawable.paint.color = Color.parseColor(rectangleColor)
+        shapeDrawable.draw(canvas)
+
+        // oval positions
+        left = 100
+        top = 400
+        right = 600
+        bottom = 550
+
+        // draw oval shape to canvas
+        shapeDrawable = ShapeDrawable(OvalShape())
+        shapeDrawable.setBounds( left, top, right, bottom)
+        shapeDrawable.paint.color = Color.parseColor(ovalColor)
+        shapeDrawable.draw(canvas)
+
+        // set bitmap as background to ImageView
+        val view = findViewById<View>(R.id.view)
+        view.background = BitmapDrawable(resources, bitmap)
+    }
+
+
 }
--- app/src/main/java/com/summers/artworkeditor/NewCanvas.kt
+++
@@ -1,8 +0,0 @@
-package com.summers.artworkeditor
-
-import android.content.Context
-import android.graphics.Canvas
-
-class NewCanvas(context: Context): Canvas() {
-
-}
