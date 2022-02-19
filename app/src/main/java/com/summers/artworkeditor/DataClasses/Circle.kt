3d513907c5dd7a8f07eb5f66b84354a60a3ce17c
charlottesummers16 <48827234+charlottesummers16@users.noreply.github.com>
2022-02-19 19:59:31 +0000

    WIP: add data classes.

---
+++ app/src/main/java/com/summers/artworkeditor/DataClasses/Circle.kt
@@ -0,0 +1,7 @@
+package com.summers.artworkeditor.DataClasses
+
+import android.graphics.Color
+
+data class Circle: Object(val x: Float, val y: Float, val colour: Color) {
+
+}
---
+++ app/src/main/java/com/summers/artworkeditor/DataClasses/Object.kt
@@ -0,0 +1,3 @@
+package com.summers.artworkeditor.DataClasses
+
+data class Object()
